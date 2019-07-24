a8d6ab270f24930bf566716513aea40c78a8a8d0
Adil Khan <40705470+adilkhan4@users.noreply.github.com>
2019-07-24 13:49:05 +0530

    Add files via upload

--- pwdscript.js
+++ pwdscript.js
@@ -1,14 +1,12 @@
-
 function check(form)
 {
 
 if(form.userid.value == "Adil Khan" && form.pwd.value == "Adil@1004")
 {
-	return true;
-}
-else
-{
-	alert("Incorrect Username or Password")
-	return false;
-}
+    window.open('resumedisplay.html', '_self');
+  }
+ else
+ {
+   alert("Incorrect Username or Password")
+  }
 }
